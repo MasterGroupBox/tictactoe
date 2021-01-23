adb3bd4da78747a5225b11c82c75515d0bae000a
Ahmed Benziada <abenziada.mirage@gmail.com>
2021-01-23 20:55:50 +0100

    CheckWinner fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // Uncomment de mode that you'd like to run
 for(let i=1; i<=9; i++){
     //Mode 1 : 2 joueurs
-        //document.getElementById("p"+i).addEventListener("click",function(){xo(i-1)});
+        // document.getElementById("p"+i).addEventListener("click",function(){xo(i-1)});
     
     //Mode 2 : AI imbattable | Pas encore au point
         document.getElementById("p"+i).addEventListener("click",function(){play(i)});
@@ -21,8 +21,8 @@ document.getElementById("button").addEventListener("click", function(){
 let count = 0;
 let logMsg = "Log :<br><br>";
 const zero = [0,0,0,
-    0,0,0,
-    0,0,0];
+            0,0,0,
+            0,0,0];
 let board = [...zero];
 
 log("");
@@ -77,6 +77,9 @@ function playRandom(i){
 
 //play hard AI as per minimax algorithm
 function play(i){
+    if(board[i-1]!=0)
+        return;
+    
     board[i-1]="X";
     /*setTimeout(function(){
         alert("p");
@@ -97,36 +100,37 @@ function play(i){
 }
 
 // Fonction récursive minimax
-function mm(checkBoard, player){
+const mm = (checkBoard, player) => {
     let results = [];
     let winner = checkWinner(checkBoard);
     let playable = playablePositions(checkBoard);
     let workBoard = [...checkBoard]; // ERROR 2 FIXED : make a copy of checkboard so it doesn't get altered (by value not by ref)
 
+    console.log("Minimax | Size : "+playable.length);
     //Condition de fin - ERROR 1 FIXED : return object {position:0,score:10} instead of only the value of the score (10)
-    if(playable.length == 0 || winner!=0)
-        switch(winner){
-            case "O":
-                return {position:0,score:10};
-            case "X":
-                return {position:0,score:-10};
-            default:
-                return {position:0,score:0};
-        }
-    else{
-        //Sinon, calculer le score de chaque case vide par appel de minimax
-        for(let i=0;i<playable.length;i++){
-            workBoard[playable[i]]=player;
-            log("Testing position "+playable[i]+" for "+player);
-            results.push({position:playable[i],score:mm(workBoard, (player=="X")? "O":"X").score}); // ERROR 3 FIXED : position:playable[i] instead of position:i
-        }
 
-        //Aprés avoir collecté les scores, retourner le min si c'est le tour de l'humain sinon le max
-        if(player == "X")
-            return min(results);
-        else
-            return max(results);
+    if(winner == "O")
+        return {position:0,score:10};
+    else if(winner == "X")
+        return {position:0,score:-10};
+    else if(playable.length==0)
+        return {position:0,score:0};
+
+
+    //Sinon, calculer le score de chaque case vide par appel de minimax
+    for(let i=0;i<playable.length;i++){
+        workBoard[playable[i]]=player;
+        console.log("Playable of workboard = "+playablePositions(workBoard).length);
+        // log("Testing position "+playable[i]+" for "+player);
+        results.push({position:playable[i],score:mm(workBoard, otherPlayer(player)).score}); // ERROR 3 FIXED : position:playable[i] instead of position:i
+        workBoard[playable[i]]=0;
     }
+
+    //Aprés avoir collecté les scores, retourner le min si c'est le tour de l'humain sinon le max
+    if(player == "X")
+        return min(results);
+    else
+        return max(results);
 }
 
 function min(results){
@@ -172,21 +176,21 @@ function playablePositions(checkBoard){
 // Vérification du gagnant
 function checkWinner(checkBoard){
     let winner = 0;
-    if(checkBoard[0]==checkBoard[1] && checkBoard[0]==checkBoard[2])
+    if(checkBoard[0]==checkBoard[1] && checkBoard[0]==checkBoard[2] && checkBoard[0]!=0)
         winner=checkBoard[0];
-    else if(checkBoard[3]==checkBoard[4] && checkBoard[3]==checkBoard[5])
+    else if(checkBoard[3]==checkBoard[4] && checkBoard[3]==checkBoard[5] && checkBoard[3]!=0)
         winner=checkBoard[3];
-    else if(checkBoard[6]==checkBoard[7] && checkBoard[6]==checkBoard[8])
+    else if(checkBoard[6]==checkBoard[7] && checkBoard[6]==checkBoard[8] && checkBoard[6]!=0)
         winner=checkBoard[6];
-    else if(checkBoard[0]==checkBoard[3] && checkBoard[0]==checkBoard[6])
+    else if(checkBoard[0]==checkBoard[3] && checkBoard[0]==checkBoard[6] && checkBoard[0]!=0)
         winner=checkBoard[0];
-    else if(checkBoard[1]==checkBoard[4] && checkBoard[1]==checkBoard[7])
+    else if(checkBoard[1]==checkBoard[4] && checkBoard[1]==checkBoard[7] && checkBoard[1]!=0)
         winner=checkBoard[1];
-    else if(checkBoard[2]==checkBoard[5] && checkBoard[2]==checkBoard[8])
+    else if(checkBoard[2]==checkBoard[5] && checkBoard[2]==checkBoard[8] && checkBoard[2]!=0)
         winner=checkBoard[2];
-    else if(checkBoard[0]==checkBoard[4] && checkBoard[0]==checkBoard[8])
+    else if(checkBoard[0]==checkBoard[4] && checkBoard[0]==checkBoard[8] && checkBoard[0]!=0)
         winner=checkBoard[0];
-    else if(checkBoard[2]==checkBoard[4] && checkBoard[2]==checkBoard[6])
+    else if(checkBoard[2]==checkBoard[4] && checkBoard[2]==checkBoard[6] && checkBoard[2]!=0)
         winner=checkBoard[2];
 
     return winner;
